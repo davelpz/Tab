77dd7289a66fb34ba3829d9ddd505f7650ddc58a
David Lopez <davelpz@gmail.com>
2012-02-16 16:36:28 -0500

    Coffee/jquery widget conversion

---
+++ src/script/Tab.js
@@ -0,0 +1,35 @@
+(function() {
+  var ccTab;
+
+  window.ccTab = ccTab = (function() {
+
+    function ccTab() {}
+
+    ccTab.prototype._create = function() {
+      var $element, $tactive, $tbar, $tcontent;
+      $element = $(this.element);
+      $tcontent = $element.find('div.TabContent');
+      $tbar = $element.find('div.TabBar');
+      $tactive = $element.find('div.TabActive');
+      $tcontent.find('div').hide().filter(':first').show();
+      return $tbar.find('a').click(function() {
+        $tcontent.find('div').hide();
+        $tcontent.find('div').filter(this.hash).show();
+        $tactive.find('a.TabActiveLink').text($(this).text());
+        return $tactive.css({
+          "left": $(this).parent().position().left
+        });
+      });
+    };
+
+    ccTab.prototype.destroy = function() {
+      return $(this.element).find('div.TabBar a').unbind('click');
+    };
+
+    return ccTab;
+
+  })();
+
+  $.widget("cc.ccTab", new ccTab);
+
+}).call(this);
